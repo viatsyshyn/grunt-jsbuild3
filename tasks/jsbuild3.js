d793b85568b32b2d397016592d4ed228c57d202f
Volodymyr Iatsyshyn <viatsyshyn@hellowebapps.com>
2015-09-01 23:28:40 +0300

    fixed async

--- tasks/jsbuild3.js
+++ tasks/jsbuild3.js
@@ -12,12 +12,12 @@ var JsBuild3 = require('emp.ria-jsbuild3');
 
 module.exports = function (grunt) {
 
-    var done = this.async();
-
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
     grunt.registerMultiTask('jsbuild3', 'EMP RIA Jsbuild3 Gruntjs plugin', function () {
+        var done = this.async();
+
         // Merge task-specific and/or target-specific options with these defaults.
         var ARGS = this.options({
             config: 'jsbuild.json',
